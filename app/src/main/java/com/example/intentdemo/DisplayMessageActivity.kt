9dc314337827d726739dafb05f79bed5172cb84d
ratul-space <to.ratul.hosen@gmail.com>
2024-01-07 08:16:43 +0600

    feat(DisplayMessageActivity): get the value via intent

--- app/src/main/java/com/example/intentdemo/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/intentdemo/DisplayMessageActivity.kt
@@ -1,11 +1,15 @@
 package com.example.intentdemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class DisplayMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
+        val message = intent.getStringExtra("com.example.intent.demo.MY_MESSAGE")
+        Log.d("DisplayMessageActivity", "message: $message")
     }
 }
