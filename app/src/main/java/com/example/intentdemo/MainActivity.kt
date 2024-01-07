e44200e7bf0d8e382ffdbcb10cf16a343ccf391d
ratul-space <to.ratul.hosen@gmail.com>
2024-01-07 11:19:10 +0600

    feat(MainActivity): add multiple button

--- app/src/main/java/com/example/intentdemo/MainActivity.kt
+++ app/src/main/java/com/example/intentdemo/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.intentdemo
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
+        val TAG = "MainActivity"
         const val MY_MESSAGE = "com.example.intent.demo.MY_MESSAGE"
     }
 
@@ -17,32 +22,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnSend = findViewById<Button>(R.id.btnSend)
-        btnSend.setOnClickListener {
-            val intent = Intent(applicationContext, DisplayMessageActivity::class.java)
-            val message = findViewById<EditText>(R.id.etPhoneNumber).text.toString()
-            intent.putExtra(MY_MESSAGE, message)
-            startActivity(intent)
-        }
+        findViewById<Button>(R.id.btnSend).setOnClickListener(ButtonClickListener)
+        findViewById<Button>(R.id.btnImplicitTelephone).setOnClickListener(ButtonClickListener)
+        findViewById<Button>(R.id.btnImplicitCustom).setOnClickListener(ButtonClickListener)
+
+//        val btnSend = findViewById<Button>(R.id.btnSend)
+//        btnSend.setOnClickListener {
+//            val intent = Intent(applicationContext, DisplayMessageActivity::class.java)
+//            val message = findViewById<EditText>(R.id.etPhoneNumber).text.toString()
+//            intent.putExtra(MY_MESSAGE, message)
+//            startActivity(intent)
+//        }
     }
 
-    /*private val ButtonClickListener: (View) -> Unit = { view ->
+    private val ButtonClickListener: (View) -> Unit = { view ->
         when (view.id) {
-            R.id.btn_sent -> {
+            R.id.btnSend -> {
                 val displayMessageIntent =
                     Intent(applicationContext, DisplayMessageActivity::class.java)
-                val massege = findViewById<EditText>(R.id.etPhoneNumber).text.toString()
-                displayMessageIntent.putExtra(MY_massage, massege)
+                val message = findViewById<EditText>(R.id.etPhoneNumber).text.toString()
+                displayMessageIntent.putExtra(MY_MESSAGE, message)
                 startActivity(displayMessageIntent)
             }
 
-            R.id.btn_implicit_telephone -> {
-                val phoneNumber: Uri.parse("tel:+165738287386")
+            R.id.btnImplicitTelephone -> {
+                val phoneNumber: Uri = Uri.parse("tel:+165738287386")
                 val dialIntent: Intent = Intent(Intent.ACTION_DIAL, phoneNumber)
+                startActivity(dialIntent)
 
+                Log.d(TAG, "btnImplicitTelephone: Clicked")
+            }
 
-                Log(TAG: )
+            R.id.btnImplicitCustom -> {
+                val customIntent = Intent("dummy.intent.action.MY_ACTION")
+                startActivity(customIntent)
+                Log.d(TAG, "btnImplicitCustom: Clicked")
             }
         }
-    }*/
+    }
 }
