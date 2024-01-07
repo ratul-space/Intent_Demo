bfbef099898439525a43490da7352a6928d19043
ratul-space <to.ratul.hosen@gmail.com>
2024-01-07 11:18:04 +0600

    feat(DisplayMessageActivity): add txv to the screen

--- app/src/main/java/com/example/intentdemo/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/intentdemo/DisplayMessageActivity.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 
 class DisplayMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
         val message = intent.getStringExtra("com.example.intent.demo.MY_MESSAGE")
-        Log.d("DisplayMessageActivity", "message: $message")
+        val txvMessage = findViewById<TextView>(R.id.txv_message)
+        txvMessage.text = message
+//        Log.d("DisplayMessageActivity", "message: $message")
     }
 }
